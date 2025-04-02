1278a7cb9fa00a0541bf27ac1becff1c8b69f166
JosephineBira <josephinebira3@gmail.com>
2025-04-02 15:39:07 +0100

    commit 3#
    In nv, removed the private key word to make the class default

--- src/userData.java
+++ src/userData.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 public class userData {
-     private static final String url = "jdbc:mysql://localhost:3306/datainput_db";
+      static final String url = "jdbc:mysql://localhost:3306/datainput_db";
     private static final String user = "root";
     private static final String password = "";
 
